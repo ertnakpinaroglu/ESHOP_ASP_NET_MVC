c1ddeaf6547301a39ff204c482c8156feb5414f8
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 22:56:53 +0300

    SubCategory class  is created

---
+++ EShop.EntitiesLayer/Entities/SubCategory.cs
@@ -0,0 +1,17 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class SubCategory:BaseEntity,IEntity
+    {
+        public int SubCategoryId { get; set; }
+        public String SubCategoryName { get; set; }
+        public String SubCategoryDescription { get; set; }
+
+    }
+}
