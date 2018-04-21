1250b29f9e6012b658d02a21afda2408c114de5e
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 23:03:43 +0300

    Customer table is created

---
+++ EShop.EntitiesLayer/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class Customer:BaseEntity,IEntity
+    {
+        public int CustomerId { get; set; }
+        public String CustomerName { get; set; }
+        public String CustomerSurname { get; set; }
+        public String Email { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+
+    }
+}
