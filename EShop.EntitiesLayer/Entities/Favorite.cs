28bf55e60047cf5f372e02f2c15a564890aeca2f
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 23:08:55 +0300

    Favorite class is created

---
+++ EShop.EntitiesLayer/Entities/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class Favorite:BaseEntity,IEntity
+    {
+        public int FavoriteId { get; set; }
+
+    }
+}
