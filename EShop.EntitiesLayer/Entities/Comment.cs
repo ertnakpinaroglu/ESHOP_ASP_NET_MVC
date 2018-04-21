cf1b63ca9aedee8ffb11f257a33aeab27133d6e5
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 23:05:55 +0300

    Comment table is created

---
+++ EShop.EntitiesLayer/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class Comment:BaseEntity,IEntity
+    {
+        public int CommentId { get; set; }
+        public String Text { get; set; }
+
+        public String IsActive { get; set; }
+
+
+    }
+}
