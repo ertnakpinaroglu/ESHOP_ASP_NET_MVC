782ae9d8ae0f7d66384462764c46ed037e4e8b9a
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 23:18:37 +0300

    Basket Of Customer table is created

---
+++ EShop.EntitiesLayer/Entities/BasketOfCustomer.cs
@@ -0,0 +1,15 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class BasketOfCustomer:BaseEntity,IEntity
+    {
+        public int BasketId { get; set; }
+
+    }
+}
