5007425389de09b633b40e39071aee201b3d4a65
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-21 22:53:00 +0300

    Category table is created

---
+++ EShop.EntitiesLayer/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class BaseEntity
+    {
+        public String CreatedDate { get; set; }
+        public String ModifiedDate { get; set; }
+        public String RemovedDate { get; set; }
+
+    }
+}
---
+++ EShop.EntitiesLayer/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using EShop.CoreLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.EntitiesLayer.Entities
+{
+    public class Category:BaseEntity,IEntity
+    {
+        public int CategoryId { get; set; }
+        public String CategoryName { get; set; }
+        public String CategoryDescription { get; set; }
+
+
+    }
+}
