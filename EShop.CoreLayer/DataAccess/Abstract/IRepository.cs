2cf534148f61f6856e33cd78d5e80f1ae31f513b
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2018-04-24 14:02:32 +0300

    Generic Repository is ready in CoreLayer

---
+++ EShop.CoreLayer/DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.CoreLayer.DataAccess.Abstract
+{
+    public interface IRepository<TEntity> where TEntity:class,new()
+    {
+        // ortak kullanılan metodlar
+        void AddEntity(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void DeleteEntity(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        TEntity FindEntity(Expression<Func<TEntity, bool>> filter);
+
+        List<TEntity> FindRange(Expression<Func<TEntity, bool>> filter = null);
+        
+
+    }
+}
---
+++ EShop.CoreLayer/DataAccess/Concreate/Repository.cs
@@ -0,0 +1,53 @@
+﻿using EShop.CoreLayer.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Data.Entity;
+
+namespace EShop.CoreLayer.DataAccess.Concreate
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        protected DbContext _context; // DI
+        private DbSet<TEntity> _dbSet;
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+       
+
+        public void AddEntity(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public void DeleteEntity(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public TEntity FindEntity(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.SingleOrDefault(filter);
+        }
+
+        public List<TEntity> FindRange(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return _dbSet.Where(filter).ToList();
+        }
+    }
+}
